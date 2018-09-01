6b30a7fef0940d1fdeb835208a7baae195486da9
Jagannath Mishra <42279500+Jagannath-Mishra@users.noreply.github.com>
2018-09-01 15:41:43 +0530

    Manage Dependencies Constructor and setter injection.

--- SpringDemo/src/main/java/com/spring/indepth/SpringDemo/BinarySearchImpl.java
+++ SpringDemo/src/main/java/com/spring/indepth/SpringDemo/BinarySearchImpl.java
@@ -9,11 +9,25 @@ public class BinarySearchImpl {
 		@Autowired
 		private SortAlgorithm sortAlgorithm;
 		
-		BinarySearchImpl(SortAlgorithm sortAlgorithm){
+		
+		//Constructor Injection ( For mandatory Dependency)
+		/*BinarySearchImpl(SortAlgorithm sortAlgorithm){
 			
 			this.sortAlgorithm = sortAlgorithm;
-		}
+		}*/
+		//in debug log: Autowiring by type from bean name 'binarySearchImpl' via constructor
 
+		//Setter Injection ( Optional dependency )
+		/*public void setSortAlgorithm(SortAlgorithm sortAlgorithm) {
+			this.sortAlgorithm = sortAlgorithm;
+		}*/
+		//In Debug Log: Autowiring by type from bean name 'binarySearchImpl' to bean named 'quickSortAlgo'
+		
+		
+		// No constructor and no sertter
+		//  log : Autowiring by type from bean name 'binarySearchImpl' to bean named 'quickSortAlgo'
+		
+		
 		public int binarySearch(int[] numbers, int numberToSearchFor) {
 			
 			int[] sortedNumber = sortAlgorithm.sort(numbers);
@@ -23,4 +37,6 @@ public class BinarySearchImpl {
 		// Sorting an array
 		// Search the arrya
 		// Return result
+
+		
 }
